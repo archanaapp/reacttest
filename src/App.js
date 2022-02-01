27ac954006085cf3f8d6b8725e906873c17cb614
archanaappscrip <archanak@appscrip.co>
2022-02-01 16:18:36 +0530

    state in class counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
+
 import './App.css';
-import {useState} from 'react';
+import React,{Component} from 'react';
 
 
-function App() {
+class  App extends Component{
 
-  const [data,setData] = useState(0)
-
-  function updateData(){
-    setData(data + 1)
+  constructor(){
+    super(); 
+    this.state = {
+      data: 1
+    }
   }
-     return (
-    <div className="App">
-      <h1>state in functional component</h1>
-    <h1>{data}</h1>
-    <button onClick={updateData}>Update data</button> <br></br>   
-      
-    </div>
-  );
+
+  updatedata() {
+    alert("function called");
+    this.setState({data:this.state.data+1})
+  }
+  render () {
+    return(
+      <div className='App'>
+        <h1>State in class component</h1>
+        <h2>{this.state.data}</h2>
+        <button onClick={()=>this.updatedata()}>Update Data</button>
+      </div>
+    )
+  }
 }
 
 export default App;
